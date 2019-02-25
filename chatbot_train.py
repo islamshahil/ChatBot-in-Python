0cbe5fecc0e87fce2241c6fb6979472cc11eb55c
Pramit-Karmakar <47589544+Pramit-Karmakar@users.noreply.github.com>
2019-02-25 14:34:27 +0530

    Weather Module Integrated
    Weather can be displayed now by the bot.Google Weather API has been used for completing this func.

--- chatbot_train.py
+++ chatbot_train.py
@@ -3,6 +3,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 import os
 import logging 
+import requests
 
 logger = logging.getLogger() 
 logger.setLevel(logging.CRITICAL)
@@ -12,8 +13,7 @@ bot = chatbot = ChatBot('Bot',
     [
         "chatterbot.logic.BestMatch" ,
         "chatterbot.logic.MathematicalEvaluation",
-        "chatterbot.logic.TimeLogicAdapter",
-
+        "chatterbot.logic.TimeLogicAdapter",        
     ],
     input_adapter="chatterbot.input.VariableInputTypeAdapter",
     output_adapter="chatterbot.output.OutputAdapter"
@@ -27,10 +27,19 @@ for files in os.listdir('E:/ChatBot-in-Python-master/english/'):
 
 while True:
     message = input('You : ')
+    
         
     if message.strip() == 'Bye' or message.strip() == 'bye' :
-        print('PKMKB : Bye')
+        print('Bot : Bye')
         break
     if message.strip() != 'bye' or message.strip() != 'Bye'  :
         reply = bot.get_response(message)
-        print('PKMKB :',reply)
+        print('Bot : ',reply)
+
+    if message.strip() == 'weather' or message.strip() == 'Weather' or message.strip() == 'What is the Weather?' or message.strip() == 'Weather today' or message.strip() == "What's the Weather" :
+        city = input('City Name : ')
+        api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+        url = api_address + city
+        json_data = requests.get(url).json()
+        format_add = json_data['main']
+        print('Bot : ',format_add)
